57e34b26a41ae5cabe35881428dab2f52d8dbdad
bill dirks <bill.dirks@google.com>
2015-10-24 23:05:22 -0700

    Cleanup code.

--- hello.py
+++ hello.py
@@ -2,9 +2,6 @@
 
 import argparse
 
-def hello():
-  print("Hello World!")
-
 class Hello(object):
   def __init__(self, name='world'):
     self.name = name
@@ -16,6 +13,5 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Hello greeter')
   parser.add_argument('--name', default="World", help='Name to greet')
   args = parser.parse_args()
-  hello()
   h = Hello(args.name)
   h.hello()
