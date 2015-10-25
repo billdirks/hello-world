8882d9915f1e95e44ad78321c847d9b800fc4323
bill dirks <bill.dirks@google.com>
2015-10-24 23:04:48 -0700

    Add hello class.

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import argparse
+
 def hello():
   print("Hello World!")
 
@@ -11,6 +13,9 @@ class Hello(object):
     print("Hello %s!" % self.name)
 
 if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Hello greeter')
+  parser.add_argument('--name', default="World", help='Name to greet')
+  args = parser.parse_args()
   hello()
-  h = Hello('George')
+  h = Hello(args.name)
   h.hello()
